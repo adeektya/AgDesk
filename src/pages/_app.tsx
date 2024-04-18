d4323bd623e9779fd87179fa993e7833ba74d6c6
Aditya Krishna <adityakrishna31@gmail.com>
2024-04-18 15:29:16 +1000

    new files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+// pages/_app.tsx
 import "@/styles/globals.css";
+import "@/styles/satoshi.css";
 import type { AppProps } from "next/app";
+import RootLayout from "@/components/RootLayout/RootLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
 }
